fcb08f638a6c0c991e42057e9c13caf714c09ef0
araymer <aaronraymer26@gmail.com>
2015-01-12 12:20:49 -0700

    Added item types for Inventory so that we can control slot assignment

---
+++ DnD5eGenerator/src/Items/Shield.java
@@ -0,0 +1,11 @@
+package Items;
+
+
+public class Shield extends Armor {
+
+	public Shield(String n, double w, Size s) {
+		super(n, w, s);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ DnD5eGenerator/src/Items/Weapon.java
@@ -0,0 +1,14 @@
+package Items;
+
+
+public class Weapon extends Item {
+	
+	private int damageDie;
+	private WeaponType weaponType;
+	
+	public Weapon(String n, double w, Size s) {
+		super(n, w, s);
+		// TODO Auto-generated constructor stub
+	}
+
+}
